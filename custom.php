ae3936d37e89b5ab58be99d72a52c863c975411a
Anna Michelle <anna_michelle@msn.com>
2014-10-10 11:06:14 -0500

    Deleted extra explanatory comments

--- custom.php
+++ custom.php
@@ -1,21 +1,20 @@
 <?php 
 
-function emiglio_exhibit_builder_page_nav($exhibitPage = null) // creates exhibit builder page nav function, sets default $exhibitPage to null
+function emiglio_exhibit_builder_page_nav($exhibitPage = null)
 {
-    if (!$exhibitPage) { // if $exhibitPage is false or null, uses the current page
+    if (!$exhibitPage) {
         if (!($exhibitPage = get_current_record('exhibit_page', false))) {
             return;
         }
     }
 
-    $exhibit = $exhibitPage->getExhibit(); // sets variable $exhibit to the page's owning exhibit if $exhibitPage was NOT empty
-    $html = '<ul class="exhibit-page-nav navigation" id="secondary-nav">' . "\n";   // sets $html variable to unorderd list with classes exhibit-page-nav and navigation, and id secondary-nav ; \n is a line break
-    $pages = $exhibit->getTopPages(); // sets variable $pages to all the pages with no parent
+    $exhibit = $exhibitPage->getExhibit();
+    $html = '<ul class="exhibit-page-nav navigation" id="secondary-nav">' . "\n";
+    $pages = $exhibit->getTopPages();
     $htmlChild = '';
-    foreach ($pages as $page) { // for each of these top-level pages...
-        $current = (exhibit_builder_is_current_page($page)) ? 'class="current"' : ''; // variable $current is set to "current" class if it's the current page, otherwise the variable is empty
-        $html .= "<li $current>" . exhibit_builder_link_to_exhibit($exhibit, $page->title, array(), $page); // added to the $html variable is a list element (with current class, if applicable) with a link to that page
-        // The following function shows the child pages of the current top-level section
+    foreach ($pages as $page) {
+        $current = (exhibit_builder_is_current_page($page)) ? 'class="current"' : '';
+        $html .= "<li $current>" . exhibit_builder_link_to_exhibit($exhibit, $page->title, array(), $page);
         if ($current) {
             if ($page->countChildPages() > 0) {
                 $childPages = $page->getChildPages();
@@ -42,8 +41,7 @@ function emiglio_exhibit_builder_page_nav($exhibitPage = null) // creates exhibi
                 }
             }
         }
-
-        $html .= $htmlChild . '</li>'; // adds closing li tag to $html
+        $html .= $htmlChild . '</li>';
         $htmlChild = '';
     }
     $html .= '</ul>' . "\n";
