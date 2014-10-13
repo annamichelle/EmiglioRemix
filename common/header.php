ae2a7e1e319c9d167221ab5380a2f270592ea8f4
Anna Michelle <anna_michelle@msn.com>
2014-10-13 14:03:52 -0500

    deleted extra space

--- common/header.php
+++ common/header.php
@@ -45,7 +45,6 @@
             <?php fire_plugin_hook('public_header', array('view'=>$this)); ?>
 
             <div id="site-title"><?php echo link_to_home_page(theme_logo()); ?></div>
-
             
             <div id="primary-nav">
                 <nav id="top-nav">
