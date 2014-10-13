1d8acf45c76bb69aba7a4f4e46bd9424c0b102b1
Anna Michelle <anna_michelle@msn.com>
2014-10-13 15:39:11 -0500

    Updated colors and rearranged homepage elements
    Gave color differences more contrast for featured
    items/collections/exhibits

--- index.php
+++ index.php
@@ -1,13 +1,32 @@
 <?php echo head(array('bodyid'=>'home')); ?>
 
-<?php if ($homepageText = get_theme_option('Homepage Text')): ?>
+<div id="primary">
+
+    <?php if ($homepageText = get_theme_option('Homepage Text')): ?>
     <!-- Homepage Text -->
     <div id="intro">
     <?php echo $homepageText; ?>
     </div>
-<?php endif; ?>
+    <?php endif; ?>
 
-<div id="primary">
+    <?php if (get_theme_option('Display Featured Collection') !== '0'): ?>
+    <!-- Featured Collection -->
+    <div id="featured-collection" class="featured">
+        <h2><?php echo __('Featured Collection'); ?></h2>
+        <?php echo random_featured_collection(); ?>
+    </div><!-- end featured collection -->
+    <?php endif; ?>
+
+    <?php if ((get_theme_option('Display Featured Exhibit') !== '0')
+           && plugin_is_active('ExhibitBuilder')
+           && function_exists('exhibit_builder_display_random_featured_exhibit')): ?>
+    <!-- Featured Exhibit -->
+    <?php echo exhibit_builder_display_random_featured_exhibit(); ?>
+    <?php endif; ?>
+
+</div>
+
+<div id="secondary">
 
     <?php if (get_theme_option('Display Featured Item') !== '0'): ?>
     <!-- Featured Item -->
@@ -33,22 +52,6 @@
     </div><!--end recent-items -->
     <?php endif; ?>
 
-</div>
-<div id="secondary">
-    <?php if (get_theme_option('Display Featured Collection') !== '0'): ?>
-    <!-- Featured Collection -->
-    <div id="featured-collection" class="featured">
-        <h2><?php echo __('Featured Collection'); ?></h2>
-        <?php echo random_featured_collection(); ?>
-    </div><!-- end featured collection -->
-    <?php endif; ?>
-
-    <?php if ((get_theme_option('Display Featured Exhibit') !== '0')
-           && plugin_is_active('ExhibitBuilder')
-           && function_exists('exhibit_builder_display_random_featured_exhibit')): ?>
-    <!-- Featured Exhibit -->
-    <?php echo exhibit_builder_display_random_featured_exhibit(); ?>
-    <?php endif; ?>
 </div>
 
 <?php echo foot(); ?>
