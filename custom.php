7c4ba26deae6840fb1efdd520464d03b667a70da
Anna Michelle <anna_michelle@msn.com>
2014-10-30 12:24:12 -0500

    added appears in exhibits link
    but it lists each appearance as a separate link :(

--- custom.php
+++ custom.php
@@ -61,4 +61,25 @@ function emiglio_exhibit_builder_page_summary($exhibitPage = null)
             . '</li>';
     return $html;
 }
+
+function link_to_related_exhibits($item) {
+
+    $db = get_db();
+
+    $select = "
+    SELECT e.* FROM {$db->prefix}exhibits AS e
+    INNER JOIN {$db->prefix}exhibit_pages AS ep on ep.exhibit_id = e.id
+    INNER JOIN {$db->prefix}exhibit_page_blocks AS epb ON epb.page_id = ep.id
+    INNER JOIN {$db->prefix}exhibit_block_attachments AS epba ON epba.block_id = epb.id
+    WHERE epba.item_id = ?";
+
+    $exhibits = $db->getTable("Exhibit")->fetchObjects($select,array($item->id));
+	
+	if(!empty($exhibits)) {
+        echo '<h2>Appears in Exhibits</h2>';
+        foreach($exhibits as $exhibit) {
+            echo '<p class="element-text"><a href="/exhibits/show/'.$exhibit->slug.'">'.$exhibit->title.'</a></p>';
+        }
+    }
+}
 ?>
