7356a2b17f397702a1ddf5a57f97b5fc766f0e66
Anna Michelle <anna_michelle@msn.com>
2015-02-03 14:50:27 -0600

    updated collection show page

--- collections/show.php
+++ collections/show.php
@@ -5,6 +5,56 @@ echo head(array('title'=>metadata('collection', array('Dublin Core', 'Title')), 
 <h1><?php echo metadata('collection', array('Dublin Core', 'Title')); ?></h1>
 
 <div id="primary" class="show">
+    <?php 
+        $collectionDescription = metadata('collection', array('Dublin Core', 'Description'));
+        $collectionSource = metadata('collection', array('Dublin Core', 'Source'));
+        $collectionRights = metadata('collection', array('Dublin Core', 'Rights'));
+        $collectionFormat = metadata('collection', array('Dublin Core', 'Format'));
+        $collectionCoverage = metadata('collection', array('Dublin Core', 'Coverage'));
+    ?>
+    <?php if ($collectionDescription): ?>
+    <div id="description" class="element">
+        <h2><?php echo __('About the Collection'); ?></h2>
+        <div class="element-text"><?php echo $collectionDescription; ?></div>
+    </div><!-- end description -->
+    <?php endif; ?>
+    <?php if ($collectionCoverage): ?>
+    <div id="coverage" class="element">
+        <h3><?php echo __('Coverage'); ?></h3>
+        <div class="element-text"><?php echo $collectionCoverage; ?></div>
+    </div><!-- end coverage -->
+    <?php endif; ?>
+    <?php if ($collectionFormat): ?>
+    <div id="format" class="element">
+        <h3><?php echo __('Format'); ?></h3>
+        <div class="element-text"><?php echo $collectionFormat; ?></div>
+    </div><!-- end format -->
+    <?php endif; ?>
+    <?php if ($collectionRights): ?>
+    <div id="rights" class="element">
+        <h3><?php echo __('Rights'); ?></h3>
+        <div class="element-text"><?php echo $collectionRights; ?></div>
+    </div><!-- end Rights -->
+    <?php endif; ?>
+    <?php if ($collectionSource): ?>
+    <div id="source" class="element">
+        <h3><?php echo __('Source'); ?></h3>
+        <div class="element-text"><?php echo $collectionSource; ?></div>
+    </div><!-- end source -->
+    <?php endif; ?>
+    <?php if (metadata('collection', array('Dublin Core', 'Contributor'))): ?>
+    <div id="collectors" class="element">
+        <h2><?php echo __('Contributor(s)'); ?></h2>
+        <div class="element-text">
+            <ul>
+                <li><?php echo metadata('collection', array('Dublin Core', 'Contributor'), array('delimiter'=>'</li><li>')); ?></li>
+            </ul>
+        </div>
+    </div><!-- end collectors -->
+    <?php endif; ?>
+    <?php echo fire_plugin_hook('public_collections_show', array('view' => $this, 'collection' => $collection)); ?>
+</div>
+<div id="secondary">
     <div id="collection-items">
         <?php $collectionItems = get_records('item', array('collection' => $collectionId), 3); ?>
         <?php foreach (loop('items', $collectionItems) as $item): ?>
@@ -29,26 +79,7 @@ echo head(array('title'=>metadata('collection', array('Dublin Core', 'Title')), 
     <?php endforeach; ?>
     </div><!-- end collection-items -->
     <?php if (count($collectionItems) > 0): ?>
-    <p class="view-items-link"><?php echo link_to_items_browse(__('View the items in %s', metadata('collection', array('Dublin Core', 'Title'))), array('collection' => $collectionId)); ?></p>
+    <p class="view-items-link"><?php echo link_to_items_browse(__('View all items in %s', metadata('collection', array('Dublin Core', 'Title'))), array('collection' => $collectionId)); ?></p>
     <?php endif; ?>
-</div>
-<div id="secondary">
-    <?php if ($collectionDescription = metadata('collection', array('Dublin Core', 'Description'))): ?>
-    <div id="description" class="element">
-        <h2><?php echo __('About the Collection'); ?></h2>
-        <div class="element-text"><?php echo $collectionDescription; ?></div>
-    </div><!-- end description -->
-    <?php endif; ?>
-    <?php if (metadata('collection', array('Dublin Core', 'Contributor'))): ?>
-    <div id="collectors" class="element">
-        <h2><?php echo __('Contributor(s)'); ?></h2>
-        <div class="element-text">
-            <ul>
-                <li><?php echo metadata('collection', array('Dublin Core', 'Contributor'), array('delimiter'=>'</li><li>')); ?></li>
-            </ul>
-        </div>
-    </div><!-- end collectors -->
-    <?php endif; ?>
-    <?php echo fire_plugin_hook('public_collections_show', array('view' => $this, 'collection' => $collection)); ?>
 </div>
 <?php echo foot(); ?>
