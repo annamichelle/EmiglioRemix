3da377a2ac0317e91749ba74c6cec30f105b5539
Anna Michelle <anna_michelle@msn.com>
2015-02-06 10:29:58 -0600

    show more metadata on collection show page
    Creator, Language, Identifier, Relation

--- collections/show.php
+++ collections/show.php
@@ -11,12 +11,22 @@ echo head(array('title'=>metadata('collection', array('Dublin Core', 'Title')), 
         $collectionRights = metadata('collection', array('Dublin Core', 'Rights'));
         $collectionFormat = metadata('collection', array('Dublin Core', 'Format'));
         $collectionCoverage = metadata('collection', array('Dublin Core', 'Coverage'));
+        $collectionCreator = metadata('collection', array('Dublin Core', 'Creator'));
+        $collectionLanguage = metadata('collection', array('Dublin Core', 'Language'));
+        $collectionIdentifier = metadata('collection', array('Dublin Core', 'Identifier'));
+        $collectionRelation = metadata('collection', array('Dublin Core', 'Relation'));
     ?>
     <?php if ($collectionDescription): ?>
     <div id="description" class="element">
         <h2><?php echo __('About the Collection'); ?></h2>
         <div class="element-text"><?php echo $collectionDescription; ?></div>
     </div><!-- end description -->
+    <?php endif; ?>
+    <?php if ($collectionCreator): ?>
+    <div id="creator" class="element">
+        <h3><?php echo __('Creator'); ?></h3>
+        <div class="element-text"><?php echo $collectionCreator; ?></div>
+    </div><!-- end creator -->
     <?php endif; ?>
     <?php if ($collectionCoverage): ?>
     <div id="coverage" class="element">
@@ -30,6 +40,12 @@ echo head(array('title'=>metadata('collection', array('Dublin Core', 'Title')), 
         <div class="element-text"><?php echo $collectionFormat; ?></div>
     </div><!-- end format -->
     <?php endif; ?>
+    <?php if ($collectionLanguage): ?>
+    <div id="language" class="element">
+        <h3><?php echo __('Language'); ?></h3>
+        <div class="element-text"><?php echo $collectionLanguage; ?></div>
+    </div><!-- end language -->
+    <?php endif; ?>
     <?php if ($collectionRights): ?>
     <div id="rights" class="element">
         <h3><?php echo __('Rights'); ?></h3>
@@ -41,6 +57,18 @@ echo head(array('title'=>metadata('collection', array('Dublin Core', 'Title')), 
         <h3><?php echo __('Source'); ?></h3>
         <div class="element-text"><?php echo $collectionSource; ?></div>
     </div><!-- end source -->
+    <?php endif; ?>
+    <?php if ($collectionIdentifier): ?>
+    <div id="identifier" class="element">
+        <h3><?php echo __('Identifier'); ?></h3>
+        <div class="element-text"><?php echo $collectionIdentifier; ?></div>
+    </div><!-- end identifier -->
+    <?php endif; ?>
+    <?php if ($collectionRelation): ?>
+    <div id="relation" class="element">
+        <h3><?php echo __('Relation'); ?></h3>
+        <div class="element-text"><?php echo $collectionRelation; ?></div>
+    </div><!-- end relation -->
     <?php endif; ?>
     <?php if (metadata('collection', array('Dublin Core', 'Contributor'))): ?>
     <div id="collectors" class="element">
